62dd436ccb7424649020acfe235a90b14aeb70b7
Louis Flores <louisdefleurs@gmail.com>
2021-12-22 16:40:51 -0500

    created basic structure for application

--- src/page/LoungePage.js
+++ src/page/LoungePage.js
@@ -3,7 +3,36 @@ import './LoungePage.scss';
 
 export default function LoungePage() {
   return (
-    <h1>BrainStation Lounge</h1>
+    <main>
+      <section className="students">
+        <h2>Listening: 5</h2>
+        {/* Open live chat onClick */}
+        <div></div>
+      </section>
+      <section className="user">
+        <nav className="user__navigation">
+          {/* Modal comes out with Notes and folders */}
+          <i className="far fa-sticky-note user__navigation-icon" title="Notes"></i>
+          {/* Pomodoro time and regular timer */}
+          {/* on screen animation for timer  */}
+          <i className="far fa-clock user__navigation-icon" title="Timers"></i>
+          {/* Opens a modal of a task list*/}
+          {/* current task can be changed and shown as student activity */}
+          <i className="fas fa-tasks user__navigation-icon" title="Tasks"></i>
+          {/* Drop down menu for user settings/ customization */}
+          <i className="fas fa-user-cog user__navigation-icon" title="Settings"></i>
+        </nav>
+      </section>
+      <section className="gifs">
+        <img src="https://dc85enhu9zukf.cloudfront.net/gifs/4oHyOIBIt57ag.gif" alt="gifs" className="background-gif" />
+        {/* <img src="" alt="gifs" className="background-gif" /> */}
+      </section>
+      <section className="random-tip-container">
+      </section>
+      <section className="audio-controller">
+
+      </section>
+    </main>
   );
 }
 
