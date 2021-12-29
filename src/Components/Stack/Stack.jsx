57bd62b449b08653e77701748d40000da886444e
Daniel Stich <danielwstich@gmail.com>
2021-12-29 15:10:14 -0500

    added function for getting posts from API using axios

--- src/Components/Stack/Stack.jsx
+++ src/Components/Stack/Stack.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import { useState } from 'react/cjs/react.development'
-import './Stack.scss'
+import React from 'react';
+import { useState } from 'react/cjs/react.development';
+import './Stack.scss';
+import axios from 'axios';
 
 export default function Stack() {
     const initialSearch = {
         hasReact: true,
-        hasSASS: false,
+        hasCSS: false,
         hasNodejs: false,
         hasExpress: false,
         hasJS: false
     }
+
+    const tags = [{
+        tag: 'reactjs',
+        key: 'hasReact'
+    },
+    {
+        tag: 'CSS',
+        key: 'hasCSS'
+    },
+    {
+        tag: 'node.js',
+        key: 'hasNodejs'
+    },
+    {
+        tag: 'express',
+        key: 'hasExpress'
+    },
+    {
+        tag: 'javascript',
+        key: 'hasJS'
+    }]
 
     const [search, setSearch] = useState(initialSearch);
 
@@ -21,6 +43,26 @@ export default function Stack() {
         })
     }
 
+    const tagBuilder = tags => {
+        return tags
+            .filter(tag => {
+                if (search[tag.key]) return tag;
+            })
+            .map(tag => tag.tag)
+            .join('%3')
+    }
+
+    let from = '1638316800';
+    let to = '1640908800';
+
+    const getQuestions = (tags, from, to) => {
+        axios
+            .get(`https://api.stackexchange.com/2.3/search?page=1&pagesize=5&fromdate=${from}&todate=${to}&order=desc&sort=votes&tagged=${tags}&site=stackoverflow`)
+            .then(response => {
+                console.log(response)
+            })
+    }
+
     return (
         <div className='stack'>
             <h1 className="stack__title">Top 5 Stack Overflow Questions</h1>
@@ -30,8 +72,8 @@ export default function Stack() {
                     onClick={() => selectTag('hasReact')}
                 >React</button>
                 <button 
-                    className={search.hasSASS ? "stack__button stack__button--selected" : "stack__button"}
-                    onClick={() => selectTag('hasSASS')}
+                    className={search.hasCSS ? "stack__button stack__button--selected" : "stack__button"}
+                    onClick={() => selectTag('hasCSS')}
                 >SASS</button>
                 <button 
                     className={search.hasNodejs ? "stack__button stack__button--selected" : "stack__button"}
@@ -46,6 +88,7 @@ export default function Stack() {
                     onClick={() => selectTag('hasJS')}
                 >JS</button>
             </div>
+            <button className="stack__button" onClick={() => getQuestions(tagBuilder(tags), from, to)}>Get Posts</button>
         </div>
     )
 }
