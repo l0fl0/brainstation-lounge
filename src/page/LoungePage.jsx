9ead0073953e1da26f799ec74ba13bcaf374b608
Louis Flores <louisdefleurs@gmail.com>
2021-12-31 01:10:21 -0500

    merged develop over to usenotes

--- src/page/LoungePage.jsx
+++ src/page/LoungePage.jsx
@@ -1,66 +1,61 @@
-import './LoungePage.scss';
-import { useEffect, useState } from 'react';
-import UserNav from '../components/UserNav/UserNav';
-<<<<<<< HEAD
-import UserNotes from '../components/UserNotes/UserNotes';
-import homerSimson from "../assets/gifs/homerSimson.gif"
-=======
-import Timer from '../components/Timer/Timer';
-import Radio from '../components/Radio/Radio';
+import "./LoungePage.scss";
+import { useEffect, useState } from "react";
+import UserNav from "../components/UserNav/UserNav";
+import Timer from "../components/Timer/Timer";
+import Radio from "../components/Radio/Radio";
+import Notes from "../components/Notes/Notes";
 import CurrentTime from "../components/CurrentTime/CurrentTime";
 
->>>>>>> develop
 export default function LoungePage() {
-
-  const [isShowTimer, setShowTimer] = useState(true);
-  const [isShowRadio, setShowRadio] = useState(true);
+  const [isShowTimer, setShowTimer] = useState(false);
+  const [isShowRadio, setShowRadio] = useState(false);
+  const [isShowNotes, setShowNotes] = useState(true);
 
   const showTimer = () => {
     setShowTimer(!isShowTimer);
-  }
+  };
   const showRadio = () => {
     setShowRadio(!isShowRadio);
-  }
-
+  };
+  const showNotes = () => {
+    setShowNotes(!isShowNotes);
+  };
 
   return (
-    <main>
+    <main className="parentContainer">
+      <section className="gifs">
+        <img
+          src="https://dc85enhu9zukf.cloudfront.net/gifs/4oHyOIBIt57ag.gif"
+          alt="gifs"
+          className="background-gif"
+        />
+        {/* <img src="" alt="gifs" className="background-gif" /> */}
+      </section>
+      <section className="stackoverflow-tips"></section>
       <header className="toolbar">
-
-        <section className="toolbar__students-social">
-          <h2 className="toolbar__students-listening">Listening: 5</h2>
+        <section className="toolbar__social">
+          <h2 className="toolbar__social-listening">Listening: 5</h2>
           {/* Open live chat onClick */}
           <div></div>
         </section>
-
         <CurrentTime />
-
         <UserNav
           showTimer={showTimer}
           showRadio={showRadio}
+          showNotes={showNotes}
         />
-
       </header>
-
-      <section className="gifs">
-        <img src={homerSimson} alt="gifs" className="background-gif" />
-        {/* <img src="" alt="gifs" className="background-gif" /> */}
-      </section>
-<<<<<<< HEAD
-      <section className="center">
-        <UserNotes />
-=======
-      <section className="stackoverflow-tips">
->>>>>>> develop
-      </section>
-      <section className="timer-container">
-        {isShowTimer ? <Timer /> : <></>}
-      </section>
-      <section className='radio-container'>
-        {isShowRadio ? <Radio /> : <></>}
-      </section>
+      <div className="popup-container">
+        <section className="notes-container">
+          {isShowNotes ? <Notes /> : <></>}
+        </section>
+        <section className="timer-container">
+          {isShowTimer ? <Timer /> : <></>}
+        </section>
+        <section className="radio-container">
+          {isShowRadio ? <Radio /> : <></>}
+        </section>
+      </div>
     </main>
   );
 }
-
-
