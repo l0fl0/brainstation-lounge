a4ce597a2084cf57769034a24c5ca144d5760f10
Louis Flores <louisdefleurs@gmail.com>
2022-02-09 18:40:12 -0500

    created new file for sockets and server comments for documentation

--- server/server.js
+++ server/server.js
@@ -9,10 +9,12 @@ dotenv.config({ path: "./config/.env" });
 // initialize the application
 const app = express();
 
-// Logger for development 
+// Logger for development using cross-env to track the node environment
 if (process.env.NODE_ENV === "development") {
   app.use(morgan('dev'))
 }
 
+
+// listen default to 8000 if env variable port is taken or busy
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
