2026e07bbb4f030b5757379bdfb05dc4c533ed05
Louis Flores <louisdefleurs@gmail.com>
2022-02-09 17:44:11 -0500

    just messing around with the component getting a understanding fot the intergration

--- client/src/Components/Chat/Chat.jsx
+++ client/src/Components/Chat/Chat.jsx
@@ -1,109 +1,81 @@
-import { useState, useEffect } from 'react/cjs/react.development'
-import './Chat.scss';
-import formatTime from '../../utils/formatDate';
-import Message from '../Message/Message';
+import { useState, useEffect } from "react";
+import "./Chat.scss";
+import formatTime from "../../utils/formatDate";
+import Message from "../Message/Message";
 
 export default function Chat() {
-    const randomMessages = [{
-        user: 'Jeff',
-        text: 'This is rad'
-    },{
-        user: 'Elliot',
-        text: 'Great Vibes'
-    },{
-        user: 'Ginger',
-        text: 'lofi homer is my favorit homer'
-    },{
-        user: 'Bridget',
-        text: 'One more Pomodoro break anyone?'
-    },{
-        user: 'Bella',
-        text: 'I\'m stoked for Node.js'
-    },{
-        user: 'Kimberly',
-        text: 'the aesthetic is R e A l'
-    },{
-        user: 'Max',
-        text: 'We need more radio stations'
-    },{
-        user: 'Bart',
-        text: 'I will not useState I will not useState I will not useState'
-    }]
+	const [msgs, setMsgs] = useState([]);
+	const [msgInput, setMsgInput] = useState("");
+	let username = null;
 
-    const [msgs, setMsgs] = useState([{
-        user: 'Daniel',
-        currentUser: false,
-        text: 'Welcome to BrainStation Lounge',
-        timestamp: formatTime(Date())
-    }]);
+	const onChangeHandler = (event) => {
+		setMsgInput(event.target.value);
+	};
 
-    const [msgInput, setMsgInput] = useState();
+	// Like a componentDidMount lifecycle method
+	useEffect(() => {
+		// sets username on Component mount for chat / saves username per session
+		if (!sessionStorage.getItem("username")) {
+			username = prompt("What is your name? ");
+			sessionStorage.setItem("username", username);
+		} else {
+			username = sessionStorage.getItem("username");
+		}
+		// send welcome message when user opens chatroom
+		setMsgs([
+			...msgs,
+			{
+				text: "Welcome " + sessionStorage.getItem("username"),
+				timestamp: formatTime(Date()),
+			},
+		]);
+	}, []);
 
-    const updateInput = (event) => {
-        setMsgInput(event.target.value)
-    }
+	const addMessage = (event) => {
+		event.preventDefault();
+		// if no txt then return
+		//TODO: add regex validation for white space input
+		if (!msgInput || !event.target.chatText.value) return;
+		// user input messages sends obj after array of msgs
+		setMsgs([
+			{
+				currentUser: true,
+				text: msgInput,
+				timestamp: formatTime(Date()),
+			},
+			...msgs,
+		]);
 
-    useEffect(() => {
-        const randomTime = (Math.floor(Math.random() * 5) + 6) * 1000;
-        const id = setInterval(() => {
-            const randomIndex = Math.floor(Math.random() * randomMessages.length); 
-            const {user, text} = randomMessages[randomIndex]
-            setMsgs(prevMsgs => {
-                const newMsgs = [...prevMsgs];
-                newMsgs.unshift({
-                    user: user,
-                    currentUser: false,
-                    text: text,
-                    timestamp: formatTime(Date())
-                })
-                return newMsgs;
-            })
-            randomTime = (Math.floor(Math.random() * 5) + 6) * 1000;
-        }, randomTime)
-    }, [])
+		// sets both state and form input to empty string no empty messages
+		event.target.chatText.value = "";
+		setMsgInput("");
+	};
 
-    const addMessage = (event) => {
-        event.preventDefault();
-        const message = msgInput;
-        if (!message) return;
+	const messageBuilder = (msgs) => {
+		const messageList = msgs.map((message, i) => {
+			return <Message key={i} message={message} isSelf={message.currentUser} />;
+		});
+		return messageList;
+	};
 
-        setMsgs(prevMsgs => {
-            const newMsgs = [...prevMsgs];
-            newMsgs.unshift({
-                user: 'Daniel',
-                currentUser: true,
-                text: message,
-                timestamp: formatTime(Date())
-            })
-            event.target.chatText.value = '';
-            setMsgInput('');
-            return newMsgs;
-        })
-    }
-
-    const messageBuilder = (msgs) => {
-        const messageList = msgs.map((message, i) => {
-            return (<Message key={i} message={message} isSelf={message.currentUser}/>)
-        })
-        return messageList;
-    }
-
-    return (
-        <div className="chat">
-            <div className="chat__text">
-                {messageBuilder(msgs)}
-            </div>
-            <div className="chat__input">
-                <i className="fas fa-user" />
-                <form 
-                    onSubmit={addMessage} 
-                    className="chat__form"
-                    autoComplete="off"
-                >
-                    <input onChange={updateInput} name="chatText" className="chat__input-field" type="text" />
-                    <button type="submit" className="button button-chat">send</button>
-                </form>
-            </div>
-        </div>
-    )
+	return (
+		<div className="chat">
+			<div className="chat__text">{messageBuilder(msgs)}</div>
+			<div className="chat__input">
+				<i className="fas fa-user" />
+				<form onSubmit={addMessage} className="chat__form" autoComplete="off">
+					<input
+						onChange={onChangeHandler}
+						name="chatText"
+						id="chatText"
+						className="chat__input-field"
+						type="text"
+					/>
+					<button type="submit" className="button button-chat">
+						send
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 }
--- client/src/Components/Message/Message.jsx
+++ client/src/Components/Message/Message.jsx
@@ -1,12 +1,13 @@
-import './Message.scss'
+import "./Message.scss";
 
 export default function Message({ message, isSelf }) {
-    const {user, text, timestamp} = message;
-    return (
-        <div className={isSelf ? "message message--self": "message"}>
-            <p className="message__user">{user}</p>
-            <p className="message__text">{text}</p>
-            <p className="message__time">{timestamp}</p>
-        </div>
-    )
+	const { user, text, timestamp } = message;
+	return (
+		<div className={isSelf ? "message message--self" : "message"}>
+			<p className="message__user">
+				{user} <span className="message__time">{timestamp}</span>
+			</p>
+			<p className="message__text">{text}</p>
+		</div>
+	);
 }
