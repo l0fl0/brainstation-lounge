9a4b4ceaa28459307ccb0e6f87fa64c7bb72dd10
Louis Flores <louisdefleurs@gmail.com>
2022-02-09 19:44:23 -0500

    server clean up

--- server/server.js
+++ server/server.js
@@ -1,20 +1,43 @@
 const express = require("express");
-const morgan = require("morgan");
 const dotenv = require("dotenv");
-
+const morgan = require("morgan");
 // config
 dotenv.config({ path: "./config/.env" });
 
 
-// initialize the application
+// initialize the express server
 const app = express();
+// initialize the http server
+const http = require("http").Server(app);
+// initialize websocket to server
+const io = require("socket.io")(http);
+
 
 // Logger for development using cross-env to track the node environment
 if (process.env.NODE_ENV === "development") {
   app.use(morgan('dev'))
 }
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan('combined'))
+}
+
+//? Test socket on 
+
+//Whenever someone connects this gets executed
+io.on('connection', function (socket) {
+  console.log('A user connected');
+
+  //Whenever someone disconnects this piece of code executed
+  socket.on('disconnect', function () {
+    console.log('A user disconnected');
+  });
+});
+
+
+
+
 
 
 // listen default to 8000 if env variable port is taken or busy
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+http.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
