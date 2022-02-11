fb0c4ecbe04043a69cfa5cce0763e668b51937aa
Louis Flores <louisdefleurs@gmail.com>
2022-02-10 19:02:46 -0500

    server up and running express and socket io / Need to attach socket to endpoint

--- server/server.js
+++ server/server.js
@@ -1,47 +1,33 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
-// const morgan = require("morgan");
-// // config
-// dotenv.config({ path: "./config/.env" });
+const express = require("express");
+const http = require("http")
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+// config
+dotenv.config({ path: "./config/.env" });
 
 
-// // initialize the express server
-// const app = express();
-// // initialize the http server
-// const http = require("http").Server(app);
-
-
-// // Logger for development using cross-env to track the node environment
-// if (process.env.NODE_ENV === "development") {
-//   app.use(morgan('dev'))
-// }
-// if (process.env.NODE_ENV === "production") {
-//   app.use(morgan('combined'))
-// }
-
-
-
-// module.exports = { http }
-
-
-
-
-// // listen default to 8000 if env variable port is taken or busy
-// const PORT = process.env.PORT || 8000;
-// http.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-
-
-
-
-
+// initialize the express server
+const app = express();
+// initialize the http server
+const server = http.createServer(app)
 // initialize websocket to server
-const io = require("socket.io")(8080);
-let users = {};
+const io = require("socket.io")(server);
 
+
+// Logger for development using cross-env to track the node environment
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan('dev'))
+}
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan('combined'))
+}
+
+// store users 
+let users = {};
 
 //Whenever someone connects this gets executed
 io.on('connection', (socket) => {
-
 
   // attached to event "send-chat-message"
   socket.on("send-chat-message", message => {
@@ -55,12 +41,27 @@ io.on('connection', (socket) => {
     socket.broadcast.emit("user-connected", username)
   })
 
+  // when disconnected then delete user from list and broadcast message to the chatroom
   socket.on('disconnect', () => {
-
     console.log(users[socket.id], "left the chat")
     socket.broadcast.emit("user-disconected", users[socket.id])
     delete users[socket.id]
   });
 });
+
+
+
+
+
+// listen default to 8000 if env variable port is taken or busy
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+
+
+
+
+
+
 
 
