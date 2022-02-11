c64334580e475bfd8faeddaad9364c5f77163000
danielstich <43919186+danielstich@users.noreply.github.com>
2022-02-10 22:15:33 -0500

    created items js file to store app/item/component information... refactored code in user nav to use item to create icons

--- client/src/Components/UserNav/UserNav.jsx
+++ client/src/Components/UserNav/UserNav.jsx
@@ -1,85 +1,32 @@
 import './UserNav.scss';
 import { useState } from 'react';
+import { items } from '../../data/items';
 
 export default function UserNav(props) {
 	const { toggleItems } = props;
+	const [isShowTitles, setShowTitles] = useState({});
 
-	const [showTitleRadio, setShowTitleRadio] = useState(false);
-	const [showTitleNotes, setShowTitleNotes] = useState(false);
-	const [showTitleTimer, setShowTitleTimer] = useState(false);
-	const [showTitleTasks, setShowTitleTasks] = useState(false);
-	const [showTitleSettings, setShowTitleSettings] = useState(false);
-	const [showTitleStack, setShowTitleStack] = useState(false);
-	const [showTitleChat, setShowTitleChat] = useState(false);
-
-	const handleTitleToggle = (set, bool) => {
-		setTimeout(() => set(bool), 100);
+	const toggleTitle = (item, bool) => {
+		const bools = { ...isShowTitles };
+		bools[item] = bool;
+		setShowTitles(bools);
 	};
 
 	return (
 		<nav className='user__navigation'>
-			<div
-				className='user__icon-container'
-				onClick={() => toggleItems('chat')}
-				onMouseEnter={() => handleTitleToggle(setShowTitleChat, true)}
-				onMouseLeave={() => handleTitleToggle(setShowTitleChat, false)}>
-				<i className='fas fa-comment-dots user__icon' />
-				{showTitleChat ? <p className='user__icon-title'>Chat</p> : null}
-			</div>
-
-			<div
-				className='user__icon-container'
-				onClick={() => toggleItems('stack')}
-				onMouseEnter={() => handleTitleToggle(setShowTitleStack, true)}
-				onMouseLeave={() => handleTitleToggle(setShowTitleStack, false)}>
-				<i className='fas fa-question user__icon' />
-				{showTitleStack ? <p className='user__icon-title'>Stack Overflow</p> : null}
-			</div>
-
-			<div
-				className='user__icon-container'
-				onClick={() => toggleItems('radio')}
-				onMouseEnter={() => handleTitleToggle(setShowTitleRadio, true)}
-				onMouseLeave={() => handleTitleToggle(setShowTitleRadio, false)}>
-				<i className='fas fa-broadcast-tower user__icon' />
-				{showTitleRadio ? <p className='user__icon-title'>Radio</p> : null}
-			</div>
-
-			<div
-				className='user__icon-container'
-				onClick={() => toggleItems('notes')}
-				onMouseEnter={() => handleTitleToggle(setShowTitleNotes, true)}
-				onMouseLeave={() => handleTitleToggle(setShowTitleNotes, false)}>
-				<i className='far fa-sticky-note user__icon' />
-				{showTitleNotes ? <p className='user__icon-title'>Notes</p> : null}
-			</div>
-
-			<div
-				className='user__icon-container'
-				onClick={() => toggleItems('timer')}
-				onMouseEnter={() => handleTitleToggle(setShowTitleTimer, true)}
-				onMouseLeave={() => handleTitleToggle(setShowTitleTimer, false)}>
-				<i className='far fa-clock user__icon' />
-				{showTitleTimer ? <p className='user__icon-title'>Timer</p> : null}
-			</div>
-
-			<div
-				className='user__icon-container'
-				onClick={() => toggleItems('tasks')}
-				onMouseEnter={() => handleTitleToggle(setShowTitleTasks, true)}
-				onMouseLeave={() => handleTitleToggle(setShowTitleTasks, false)}>
-				<i className='fas fa-tasks user__icon' />
-				{showTitleTasks ? <p className='user__icon-title'>Tasks</p> : null}
-			</div>
-
-			<div
-				className='user__icon-container'
-				onClick={() => toggleItems('settings')}
-				onMouseEnter={() => handleTitleToggle(setShowTitleSettings, true)}
-				onMouseLeave={() => handleTitleToggle(setShowTitleSettings, false)}>
-				<i className='fas fa-user-cog user__icon' />
-				{showTitleSettings ? <p className='user__icon-title'>Settings</p> : null}
-			</div>
+			{items.map((item, i) => {
+				return (
+					<div
+						key={i}
+						className='user__icon-container'
+						onClick={() => toggleItems(item.name)}
+						onMouseEnter={() => toggleTitle(item.name, true)}
+						onMouseLeave={() => toggleTitle(item.name, false)}>
+						<i className={item.iconClass} />
+						{isShowTitles[item.name] ? <p className='user__icon-title'>{item.title}</p> : null}
+					</div>
+				);
+			})}
 		</nav>
 	);
 }
---
+++ client/src/data/items.js
@@ -0,0 +1,37 @@
+export const items = [
+	{
+		name: 'chat',
+		title: 'Chat',
+		iconClass: 'fas fa-comment-dots user__icon',
+	},
+	{
+		name: 'stack',
+		title: 'Stack Overflow',
+		iconClass: 'fas fa-question user__icon',
+	},
+	{
+		name: 'radio',
+		title: 'Radio',
+		iconClass: 'fas fa-broadcast-tower user__icon',
+	},
+	{
+		name: 'notes',
+		title: 'Notes',
+		iconClass: 'far fa-sticky-note user__icon',
+	},
+	{
+		name: 'timer',
+		title: 'Timer',
+		iconClass: 'far fa-clock user__icon',
+	},
+	{
+		name: 'tasks',
+		title: 'Tasks',
+		iconClass: 'fas fa-tasks user__icon',
+	},
+	{
+		name: 'settings',
+		title: 'Settings',
+		iconClass: 'fas fa-user-cog user__icon',
+	},
+];
