60741ea7b79010cb7b65a43578f16282436395a7
Louis Flores <louisdefleurs@gmail.com>
2022-02-16 16:35:17 -0500

    changing domain

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { io } from "socket.io-client";
-const SOCKET_URL = "http://localhost:8080";
+const SOCKET_URL = "https://bslounge.netlify.app";
 
 export const socket = io(SOCKET_URL);
 export const SocketContext = React.createContext();
