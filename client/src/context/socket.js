87e013056046567c1b13ded0a47760a67f3bf6b1
Louis Flores <louisdefleurs@gmail.com>
2022-02-16 16:39:10 -0500

    hot fix wrong domain

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { io } from "socket.io-client";
-const SOCKET_URL = "https://bslounge.netlify.app";
+const SOCKET_URL = "http://bsl-api.stich.pub";
 
 export const socket = io(SOCKET_URL);
 export const SocketContext = React.createContext();
