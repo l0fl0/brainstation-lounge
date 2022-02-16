2012ec7f1dee06f449e21de2af6e1072780ceffc
Louis Flores <louisdefleurs@gmail.com>
2022-02-15 19:46:00 -0500

    chat app rework fixed bug with loading multiple sockets (now soket is truely global), fixed lag issue

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,77 +1,58 @@
-import { useState, useEffect, useRef } from "react";
-import { io } from "socket.io-client";
+import { useState, useEffect, useContext, useCallback } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./Chat.scss";
 import formatTime from "../../utils/formatDate";
 import Message from "./Message/Message";
+import { SocketContext } from "../../context/socket";
 
-// set a global immutable for the socket connection
-const socket = io("http://localhost:8080");
-let messages = [];
+// store the messages in memory
 
 export default function Chat() {
-	// State
-	let [message, setMessage] = useState(0);
+	const socket = useContext(SocketContext);
+
+	let [msgs, setMsgs] = useState([]);
 	const [msgInput, setMsgInput] = useState("");
-	// Socket listeners
-	//
-	// handle incomming message display
-	socket.on("connect", () => {
-		console.log("connected");
-	});
 
-	socket.on("chat-message", (data) => {
-		messages.unshift(<Message message={data} isSelf={data.currentUser} />);
-	});
-	socket.on("disconnect", (reason) => {
-		console.log(reason);
+	const updateMessages = useCallback((message) => {
+		let incommingMessage = (
+			<Message
+				key={message.key}
+				message={message}
+				isSelf={message.currentUser}
+			/>
+		);
 
-		if (reason === "io server disconnect") {
-			// the disconnection was initiated by the server, you need to reconnect manually
-			socket.connect();
-		}
-		if (reason === "transport close") {
-			// the disconnection was initiated by the server, you need to reconnect manually
-			socket.connect();
-		}
-	});
-
-	useEffect(() => {
-		// sets username on Component mount for chat / saves username per session
-		if (!sessionStorage.getItem("username")) {
-			sessionStorage.setItem("username", prompt("What is your name? "));
-		}
-		// connect to the new user function
-		socket.emit("new-user", sessionStorage.getItem("username"));
-
-		// The return functions as the componentWillUnmount lifecycle method
-		return () => {
-			socket.disconnect();
-		};
+		setMsgs((messages) => [incommingMessage, ...messages]);
 	}, []);
 
-	const sendMessage = (event) => {
+	const broadcastMessage = useCallback(
+		(message) => {
+			// send message to server for broadcast
+			socket.emit("send-chat-message", {
+				key: message.key,
+				user: sessionStorage.getItem("username"),
+				currentUser: false,
+				text: message.text,
+				timestamp: message.timestamp,
+			});
+		},
+		[socket]
+	);
+
+	const messageHandler = (event) => {
 		event.preventDefault();
 		if (!msgInput || !event.target.chatText.value) return;
 
 		// update userview with message
 		const userMessage = {
+			key: uuidv4(),
 			currentUser: true,
 			text: msgInput,
 			timestamp: formatTime(Date()),
 		};
-		messages.unshift(
-			<Message message={userMessage} isSelf={userMessage.currentUser} />
-		);
 
-		// send message to server for broadcast
-		socket.emit("send-chat-message", {
-			user: sessionStorage.getItem("username"),
-			currentUser: false,
-			text: msgInput,
-			timestamp: formatTime(Date()),
-		});
-		// updatestate
-		// setMessage((message += 1));
+		updateMessages(userMessage);
+		broadcastMessage(userMessage);
 
 		// sets both state and form input to empty string no empty messages
 		event.target.chatText.value = "";
@@ -83,19 +64,36 @@ export default function Chat() {
 		setMsgInput(event.target.value);
 	};
 
+	useEffect(() => {
+		// sets username on component mount for chat / saves username per session
+		if (!sessionStorage.getItem("username")) {
+			sessionStorage.setItem("username", prompt("What is your name? "));
+		}
+		// send new user event
+		socket.emit("new-user", sessionStorage.getItem("username"));
+
+		// Subscribe to events
+		socket.on("chat-message", (data) => {
+			updateMessages(data);
+		});
+
+		return () => {
+			// before the component is destroyed
+			// unbind all event handlers used in this component
+			socket.disconnect();
+		};
+	}, [socket]);
+
 	return (
 		<div className="chat">
-			<div className="chat__text">
-				{messages}
-				{/* { msgs.map((message, i) => {
-					return (
-						<Message key={i} message={message} isSelf={message.currentUser} />
-					);
-				})} */}
-			</div>
+			<div className="chat__text">{msgs}</div>
 			<div className="chat__input">
 				<i className="fas fa-user" />
-				<form onSubmit={sendMessage} className="chat__form" autoComplete="off">
+				<form
+					onSubmit={messageHandler}
+					className="chat__form"
+					autoComplete="off"
+				>
 					<input
 						onChange={onChangeHandler}
 						name="chatText"
