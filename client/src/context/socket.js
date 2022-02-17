9cdfe67c0fb9e48a64258ade718dd518ab413caf
danielstich <43919186+danielstich@users.noreply.github.com>
2022-02-17 10:33:19 -0500

    added .env to frontend

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { io } from "socket.io-client";
-const SOCKET_URL = "http://localhost:8080";
+import React from 'react';
+import { io } from 'socket.io-client';
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
 
 export const socket = io(SOCKET_URL);
 export const SocketContext = React.createContext();
