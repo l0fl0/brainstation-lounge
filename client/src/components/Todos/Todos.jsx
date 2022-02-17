e826ad8ae47ee5bb3e74bf1e76acc980f643e762
Louis Flores <louisdefleurs@gmail.com>
2022-02-17 18:02:05 -0500

    added styles for todo header

--- client/src/components/Todos/Todos.jsx
+++ client/src/components/Todos/Todos.jsx
@@ -2,9 +2,27 @@ import "./Todos.scss";
 import TodoTaskList from "./TodoTaskList/TodoTaskList";
 
 export default function Todos() {
+	const newTask = (e) => {
+		e.preventDefault();
+		alert("new task", "Louis");
+	};
+	const editTasks = (e) => {
+		e.preventDefault();
+		alert("edit Tasks");
+	};
 	return (
 		<div className="todos">
-			<h1 className="todos__header">Tasks</h1>
+			<header className="todos__header">
+				<h1 className="todos__heading">Tasks</h1>
+				<div className="todos__actions">
+					<button className="todos__action" onClick={newTask}>
+						+ Task
+					</button>
+					<button className="todos__action" onClick={editTasks}>
+						<i className="fas fa-solid fa-pen" title="edit tasks"></i>
+					</button>
+				</div>
+			</header>
 			<TodoTaskList />
 		</div>
 	);
