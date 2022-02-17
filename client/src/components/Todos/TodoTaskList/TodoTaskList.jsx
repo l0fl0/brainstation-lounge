865bea70c01c72ce255efb805465c70080ca6e7a
Louis Flores <louisdefleurs@gmail.com>
2022-02-17 18:01:22 -0500

    begining to handle strikethough for object done

---
+++ client/src/components/Todos/TodoTaskList/TodoTaskList.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./TodoTaskList.scss";
+
+export default function TodoTaskList() {
+	const [todos, setTodos] = useState([
+		{
+			id: 0,
+			text: "Talk to rep from BS to become a developer",
+			selected: false,
+		},
+		{
+			id: 1,
+			text: "Talk to rep from BS to become a developer",
+			selected: true,
+		},
+	]);
+
+	const strikeTask = (id) => {
+		// strike the todo that is cheked
+		todos[id].selected = !todos[id].selected;
+		setTodos([, ...todos]);
+
+		console.log(todos);
+	};
+
+	return (
+		<ul className="task-list">
+			{todos.map((task) => (
+				<li className="task">
+					<span
+						className={task.selected ? "task__text--done" : "task__text"}
+						onClick={() => strikeTask(task.id)}
+					>
+						{task.text}
+					</span>
+
+					<div className="task__actions-container">
+						<i className="fas fa-solid fa-pen task__action"></i>
+						<i className="far fa-trash-alt task__action"></i>
+					</div>
+				</li>
+			))}
+		</ul>
+	);
+}
