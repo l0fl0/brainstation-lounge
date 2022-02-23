0b2e12d2fe281ad29da12cd7599078b7ad49cd27
Louis Flores <louisdefleurs@gmail.com>
2022-02-23 16:35:41 -0500

    testing the modal

--- client/src/components/ModalTemplate/ModalTemplate.jsx
+++ client/src/components/ModalTemplate/ModalTemplate.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 import "./ModalTemplate.scss";
 export default function ModalTemplate() {
-	return <section className="modal__container">things</section>;
+	return (
+		<form className="modal__task-form">
+			<label className="modal__task-label" htmlFor="task">
+				New Task
+			</label>
+			<textarea
+				className="modal__task-input"
+				name="task"
+				id="task"
+				placeholder="please enter new task"
+			></textarea>
+			<button className="modal__task-button">Done</button>
+		</form>
+	);
 }
