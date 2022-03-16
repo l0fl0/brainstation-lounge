faa6525ab93e616f1ca73e78d3f555ca948ce640
Louis Flores <louisdefleurs@gmail.com>
2022-03-16 00:54:04 -0400

    styled toggle button, fixed settings functionality, cleaned up props to pass from frame to frame

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -1,8 +1,13 @@
-import './Settings.scss';
-import { gifLength } from '../../data/gifs';
+import "./Settings.scss";
+import { gifLength } from "../../data/gifs";
+import { StrictMode, useEffect, useState } from "react";
 
-export default function Settings(props) {
-	const { setGifIndex, setTwelveHourFormat } = props;
+export default function Settings({
+	gifIndex,
+	setGifIndex,
+	twelveHourFormat,
+	setTwelveHourFormat,
+}) {
 	const nextGif = () => {
 		setGifIndex((prevIndex) => {
 			return (prevIndex + 1) % gifLength;
@@ -16,23 +21,40 @@ export default function Settings(props) {
 		});
 	};
 
+	useEffect(() => {
+		const CurrentSettings = {
+			// Add setting
+			twelveHourFormat: twelveHourFormat,
+			gifIndex: gifIndex,
+		};
+
+		localStorage.setItem("settings", JSON.stringify(CurrentSettings));
+		// Add setting state to watch
+	}, [gifIndex, twelveHourFormat]);
+
 	return (
-		<div className='settings-wrapper'>
-			<div className='time-button'>
-				24 hour format
-				<button
-					onClick={() => {
-						setTwelveHourFormat(false);
-					}}>
-					Yes
-				</button>
+		<div className="settings">
+			<h2 className="settings__title">Settings</h2>
+			<div className="settings__item settings__toggle">
+				24 hour Format
+				<input
+					onClick={() => setTwelveHourFormat(!twelveHourFormat)}
+					type="checkbox"
+					className="settings__toggle-switch"
+					checked={!twelveHourFormat}
+				/>
 			</div>
-			<section className='bg-selector'>
-				<i onClick={prevGif} className='fas fa-arrow-left bg-selector__icon'></i>
-				{' change your vibe '}
-				<i onClick={nextGif} className='fas fa-arrow-right bg-selector__icon'></i>
+			<section className="settings__item settings__bg-selector">
+				change vibe
+				<div className="settings__bg-selector-actions">
+					<div onClick={prevGif}>
+						<i className="fa-solid fa-arrow-left settings__bg-selector-icon"></i>
+					</div>
+					<div onClick={nextGif}>
+						<i className="fa-solid fa-arrow-right settings__bg-selector-icon"></i>
+					</div>
+				</div>
 			</section>
-			<div>Sign Out</div>
 		</div>
 	);
 }
