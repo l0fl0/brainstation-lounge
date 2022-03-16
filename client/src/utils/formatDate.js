8e6db9bd8d488452f87922cb7395432c8779966d
Louis Flores <louisdefleurs@gmail.com>
2022-03-16 00:51:35 -0400

    simplified the time format functions

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -1,15 +1,54 @@
-export default function formatTime(date) {
-    let time = new Date(date);
+export function twelveHourTime(timestamp, clock) {
+    const date = new Date(timestamp);
 
-    const hours = time.getHours();
-    const mins = time.getMinutes();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    let seconds = date.getSeconds();
     let meridiem = hours < 12 ? "AM" : "PM";
 
-    let timeString = (String(hours % 12).padStart(2, '0') === "00" ? "12" : String(hours % 12).padStart(2, '0')) + ":" + String(mins).padStart(2, '0') + " " + meridiem;
+    let hourString = `${String(hours % 12).padStart(2, '0') === "00" ? "12" : String(hours % 12).padStart(2, '0')}`;
 
-    return timeString;
+    if (clock) return `${hourString}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')} ${meridiem}`;
+
+    return `${hourString}:${String(minutes).padStart(2, '0')} ${meridiem}`;
 }
 
-export const timeFormatter = (timestamp) => {
+export function twentyFourHourTime(timestamp, clock) {
+    const date = new Date(timestamp);
 
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    if (clock) return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
 };
+
+export function getDay(timestamp) {
+    const date = new Date(timestamp);
+    let day = date.getDay();
+    switch (day) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        default:
+            day = "Saturday";
+    }
+    return day;
+}
