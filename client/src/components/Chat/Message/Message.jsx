796da4a35bf144d7e012594d292def5202c46941
Louis Flores <louisdefleurs@gmail.com>
2022-03-16 22:55:08 -0400

    fixed bug with resize from 24hr clock to 12hr clock

--- client/src/components/Chat/Message/Message.jsx
+++ client/src/components/Chat/Message/Message.jsx
@@ -11,31 +11,35 @@ export default function Message({ message, isSelf, twelveHourFormat }) {
 	if (isSelf) {
 		return (
 			<div className="message__self">
-				<p className="message__text">
-					{text}
-					<span className="message__time">
-						{twelveHourFormat
-							? twelveHourTime(timestamp)
-							: twentyFourHourTime(timestamp)}
-					</span>
-				</p>
+				<p className="message__text">{text}</p>
+				<div
+					className={`message__time ${
+						twelveHourFormat ? "message__time--meridian" : ""
+					}`}
+				>
+					{twelveHourFormat
+						? twelveHourTime(timestamp)
+						: twentyFourHourTime(timestamp)}
+				</div>
 			</div>
 		);
 	}
 
 	return (
 		<div className="message">
-			<>
+			<div>
 				<p className="message__user">{user}</p>
-				<p className="message__text">
-					{text}
-					<span className="message__time">
-						{twelveHourFormat
-							? twelveHourTime(timestamp)
-							: twentyFourHourTime(timestamp)}
-					</span>
-				</p>
-			</>
+				<p className="message__text">{text}</p>
+			</div>
+			<div
+				className={`message__time ${
+					twelveHourFormat ? "message__time--meridian" : ""
+				}`}
+			>
+				{twelveHourFormat
+					? twelveHourTime(timestamp)
+					: twentyFourHourTime(timestamp)}
+			</div>
 		</div>
 	);
 }
